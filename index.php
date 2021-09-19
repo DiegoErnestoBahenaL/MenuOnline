5fd71c691f01c0b9d9dc14887d5d2b85ca3ec8fa
leoharrisn <dbahenalopez@hotmail.com>
2021-09-18 19:46:33 -0500

    full responsive footer

--- index.php
+++ index.php
@@ -35,91 +35,98 @@
 
 
 <body>
-    <header>
-        <div class="encabezado">
-               <h1>Menú Online</h1>
-        </div>
-     
-    </header>
-    <section class="logoRestaurante">
+    <div class="wrapper">
 
-        <?php
-            error_reporting(E_ERROR | E_PARSE);
-            $nombreRestaurante = $_GET['restaurante'];
-        
-            if (isset($_GET['restaurante']))
-            { 
-                $restaurante = $_GET['restaurante'];
-                
-                include ("../apiLogin/daltysConexion.php");
-                $conn = connectDaltys();
-            
-            
-                $query = "select imagen from Restaurante where idRestaurante=$restaurante";
-               
-
-                $resultado = mysqli_query($conn, $query) or die ("Algo fallo");
-
-                while ($fila=mysqli_fetch_array($resultado)){
-                 
-                    $imagen = $fila['imagen'];
-                }
-
-                print '<img class="logoRestaurante__imagen" src="data:image/jpeg;base64,'. base64_encode($imagen) .'"/>'; 
-
-            }
-            else 
-            {
-                print "<img class="."logoRestaurante__imagen"." src="."img/DaltysLogo.png"." alt="."Logo"." >";
-
-            }
-        ?>
-        
-        <div class="logoRestaurante__texto"> 
-            <h2>¡Te damos la Bienvenida!</h2>
-        </div>
-        
-
-
-    </section>
-
-    <form id="formAJAX" action="list.php"  method="POST">
-       
-        <div class="formulario">
-
-        
-            <fieldset>
-                <legend> Ingresa tu información</legend>
-                <div class="inputs">
-                    <div class="inputs__campo">
-                        <label>Nombre</label>
-                        <input id="nombre" name="nombre" class="inputs__campo"  type="text" required >
-                    </div>
-                    <div class="inputs__campo">
-                        <label>Numero de Mesa</label>
-                        <input id="mesa" name="mesa" class="inputs__campo" type="number" required>
-                    </div>
-                    <div >
-
-                        <input type="submit" class="inputs__enviar" placehoder="Enviar">
-                     </div>
-
-                
-                </div>
-                
-            </fieldset>
-        </div>
-
-    </form>
-
-    <footer>
-
-        <div class="footer">
-            <p>Proporcionado por DALTY's Food</p>
-        </div>
-
-    </footer>
     
+        <header>
+            <div class="encabezado">
+                <h1>Menú Online</h1>
+            </div>
+        
+        </header>
+
+        <div class="contenido">
+        <section class="logoRestaurante">
+
+            <?php
+                error_reporting(E_ERROR | E_PARSE);
+                $nombreRestaurante = $_GET['restaurante'];
+            
+                if (isset($_GET['restaurante']))
+                { 
+                    $restaurante = $_GET['restaurante'];
+                    
+                    include ("../apiLogin/daltysConexion.php");
+                    $conn = connectDaltys();
+                
+                
+                    $query = "select imagen from Restaurante where idRestaurante=$restaurante";
+                
+
+                    $resultado = mysqli_query($conn, $query) or die ("Algo fallo");
+
+                    while ($fila=mysqli_fetch_array($resultado)){
+                    
+                        $imagen = $fila['imagen'];
+                    }
+
+                    print '<img class="logoRestaurante__imagen" src="data:image/jpeg;base64,'. base64_encode($imagen) .'"/>'; 
+
+                }
+                else 
+                {
+                    print "<img class="."logoRestaurante__imagen"." src="."img/DaltysLogo.png"." alt="."Logo"." >";
+
+                }
+            ?>
+            
+            <div class="logoRestaurante__texto"> 
+                <h2>¡Te damos la Bienvenida!</h2>
+            </div>
+            
+
+
+            </section>
+
+            <form id="formAJAX" action="list.php"  method="POST">
+            
+                <div class="formulario">
+
+                
+                    <fieldset>
+                        <legend> Ingresa tu información</legend>
+                        <div class="inputs">
+                            <div class="inputs__campo">
+                                <label>Nombre</label>
+                                <input id="nombre" name="nombre" class="inputs__campo"  type="text" required >
+                            </div>
+                            <div class="inputs__campo">
+                                <label>Numero de Mesa</label>
+                                <input id="mesa" name="mesa" class="inputs__campo" type="number" required>
+                            </div>
+                            <div >
+
+                                <input type="submit" class="inputs__enviar" placehoder="Enviar">
+                            </div>
+
+                        
+                        </div>
+                        
+                    </fieldset>
+                </div>
+
+            </form>
+        </div>
+        <footer>
+
+            <div class="container">
+                <p>Proporcionado por DALTY's Food</p>
+            </div>
+
+        </footer>
+
+
+    </div>
 </body>
 </html>
 <script>
